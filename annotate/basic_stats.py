470b072f6f15beed6f25bbbaf94b77afe14915e0
christofs <c.schoech@gmail.com>
2015-01-09 18:56:13 +0100

    First version of basic_stats.py

--- annotate/basic_stats.py
+++ annotate/basic_stats.py
@@ -36,85 +36,88 @@ import numpy as np
 #######################
 
 
-def basic_stats(file):
-    """ Read file, calculate data, calculate metrics."""
-    empty = pd.DataFrame()
+def basic_stats(inpath):
+    """ Read files, calculate data, calculate metrics, save results."""
+
+    ### Empty dataframe for results.
+    index = ["no_sentences", "no_types", "no_tokens", "no_tokenchars", "avg_tokenlength", "avg_sentencelength", "ttr", "yuleK"]
+    all_output = pd.DataFrame(index=index)
+    all_output = all_output.T
+    #print(all_output)
+
     ### Read text files
-    with open(file, "r") as infile:
-        textname = os.path.basename(file)[:-4]
-        plaintext = infile.read()
-        plaintext = plaintext.lower()
-        tokens = re.findall("\w+",plaintext)
-        #print(text)
-        sr_tokens = pd.Series(tokens)
-        #print(sr_tokens)
-        df_tokens = pd.DataFrame(sr_tokens)
-        #print(df_types)
-        types = Counter(tokens)
-        #print(types)
-        sr_types = pd.Series(types)
-        #print(sr_types)
-        df_types = pd.DataFrame(sr_types)
-        #print(df_types)
-        sentences =  re.split("[\.|!|?]",plaintext)
-        #print(sentences)
+    for file in glob.glob(inpath):
+        with open(file, "r") as infile:
+            textname = os.path.basename(file)[:-4]
+            plaintext = infile.read()
+            plaintext = plaintext.lower()
+            tokens = re.findall("\w+",plaintext)
+            #print(text)
+            sr_tokens = pd.Series(tokens)
+            #print(sr_tokens)
+            df_tokens = pd.DataFrame(sr_tokens)
+            #print(df_types)
+            types = Counter(tokens)
+            #print(types)
+            sr_types = pd.Series(types)
+            #print(sr_types)
+            df_types = pd.DataFrame(sr_types)
+            #print(df_types)
+            sentences =  re.split("[\.|!|?]",plaintext)
+            #print(sentences)
 
-        ### Calculate some basic statistics: tokens, types, chars
-        #print("\n------\n",textname)
-        #print("---\nSome basic statistics")
-        no_sentences = len(sentences)
-        #print("Number of sentences in", textname, ":", no_sentences)
-        no_types = len(sr_types)
-        #print("Number of types in", textname, ":", no_types)
-        no_tokens = len(sr_tokens)
-        #print("Number of tokens in", textname, ":", no_tokens)
-        no_tokenchars = 0 #Without whitespace.
-        for word in tokens:
-            tokenlength = len(word)
-            no_tokenchars = no_tokenchars + tokenlength
-        #print("Number of characters in", textname, ":", no_tokenchars)
+    ### Calculate some basic statistics: tokens, types, chars
+            #print("\n------\n",textname)
+            #print("---\nSome basic statistics")
+            no_sentences = len(sentences)
+            #print("Number of sentences in", textname, ":", no_sentences)
+            no_types = len(sr_types)
+            #print("Number of types in", textname, ":", no_types)
+            no_tokens = len(sr_tokens)
+            #print("Number of tokens in", textname, ":", no_tokens)
+            no_tokenchars = 0 #Without whitespace.
+            for word in tokens:
+                tokenlength = len(word)
+                no_tokenchars = no_tokenchars + tokenlength
+            #print("Number of characters in", textname, ":", no_tokenchars)
 
-        ### Calculate some basic derived metrics
-        #print("---\nSome derived metrics")
-        avg_tokenlength = no_tokenchars / no_tokens
-        #print("Average token length (in chars) in", textname, ":", avg_tokenlength)
-        avg_sentencelength = no_tokens / no_sentences
-        #print("Average sentence length (in tokens) in", textname, ":", avg_sentencelength)
-        ttr = no_types / no_tokens
-        #print("Type-token-ratio (TTR) for", textname, ":", ttr)
-        """
-        hapax = []
-        for word,freq in df_types:
-            if freq == 1:
-                hapax = hapax.append(word)
-        no_hapax = len(hapax)
-        print("Number of hapax legomena:", no_hapax)
-        """
-        yule1 = no_tokens
-        yule2 = int(np.sum(df_types**2))
-        #print(yule2)
-        yuleK = 10.000 * (yule2 - yule1) / yule1**2
-        #print("Yule's K characteristic for", textname, ":", yuleK)
-
-        ### Combine all values for one text, combine all texts, save as a table.
-        columns = [textname]
-        index = ["no_sentences", "no_types", "no_tokens", "no_tokenchars", "avg_tokenlength", "avg_sentencelength", "ttr", "yuleK"]
-        #print(index)
-        results = [no_sentences, no_types, no_tokens, no_tokenchars, avg_tokenlength, avg_sentencelength, ttr, yuleK]
-        #print(results)
-        output = pd.Series(results, index=index, name=textname)
-        #print(output)
+    ### Calculate some basic derived metrics
+            #print("---\nSome derived metrics")
+            avg_tokenlength = no_tokenchars / no_tokens
+            #print("Average token length (in chars) in", textname, ":", avg_tokenlength)
+            avg_sentencelength = no_tokens / no_sentences
+            #print("Average sentence length (in tokens) in", textname, ":", avg_sentencelength)
+            ttr = no_types / no_tokens
+            #print("Type-token-ratio (TTR) for", textname, ":", ttr)
+            """
+            hapax = []
+            for word,freq in df_types:
+                if freq == 1:
+                    hapax = hapax.append(word)
+            no_hapax = len(hapax)
+            print("Number of hapax legomena:", no_hapax)
+            """
+            yule1 = no_tokens
+            yule2 = int(np.sum(df_types**2))
+            #print(yule2)
+            yuleK = 10.000 * (yule2 - yule1) / yule1**2
+            #print("Yule's K characteristic for", textname, ":", yuleK)
 
 
-        all_output = empty.append(output, ignore_index=False)
-        #print(all_output)
+    ### Combine all values for one text, combine all texts, save as a table.
+            columns = [textname]
+            index = ["no_sentences", "no_types", "no_tokens", "no_tokenchars", "avg_tokenlength", "avg_sentencelength", "ttr", "yuleK"]
+            #print(index)
+            results = [no_sentences, no_types, no_tokens, no_tokenchars, avg_tokenlength, avg_sentencelength, ttr, yuleK]
+            #print(results)
+            output = pd.Series(results, index=index, name=textname)
+            #print(output)
 
-        with open("all_output.csv", "w") as outfile:
-            all_output.to_csv(outfile)
+            all_output = all_output.append(output, ignore_index=False)
+            #print(all_output)
 
-
-
-
+    with open("results.csv", "w") as outfile:
+        all_output.to_csv(outfile)
 
 
 
@@ -124,8 +127,7 @@ def basic_stats(file):
 
 
 def main(inpath):
-    for file in glob.glob(inpath):
-        basic_stats(file)
+    basic_stats(inpath)
 
 
 main('./data/*.txt')
