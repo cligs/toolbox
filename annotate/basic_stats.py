fff5e641c97cf7506539431d33323b140cb0e1b1
christofs <c.schoech@gmail.com>
2015-01-09 18:37:47 +0100

    small change

--- annotate/basic_stats.py
+++ annotate/basic_stats.py
@@ -36,10 +36,11 @@ import numpy as np
 #######################
 
 
-def basic_stats(file): 
+def basic_stats(file):
     """ Read file, calculate data, calculate metrics."""
+    empty = pd.DataFrame()
     ### Read text files
-    with open(file, "r") as infile: 
+    with open(file, "r") as infile:
         textname = os.path.basename(file)[:-4]
         plaintext = infile.read()
         plaintext = plaintext.lower()
@@ -67,34 +68,35 @@ def basic_stats(file):
         #print("Number of types in", textname, ":", no_types)
         no_tokens = len(sr_tokens)
         #print("Number of tokens in", textname, ":", no_tokens)
-        no_tokenchars = 0 #Without whitespace. 
-        for word in tokens: 
+        no_tokenchars = 0 #Without whitespace.
+        for word in tokens:
             tokenlength = len(word)
             no_tokenchars = no_tokenchars + tokenlength
         #print("Number of characters in", textname, ":", no_tokenchars)
-                
+
         ### Calculate some basic derived metrics
-        #print("---\nSome derived metrics")       
+        #print("---\nSome derived metrics")
         avg_tokenlength = no_tokenchars / no_tokens
         #print("Average token length (in chars) in", textname, ":", avg_tokenlength)
         avg_sentencelength = no_tokens / no_sentences
         #print("Average sentence length (in tokens) in", textname, ":", avg_sentencelength)
         ttr = no_types / no_tokens
         #print("Type-token-ratio (TTR) for", textname, ":", ttr)
+        """
         hapax = []
-        for word,freq in df_types: 
+        for word,freq in df_types:
             if freq == 1:
                 hapax = hapax.append(word)
         no_hapax = len(hapax)
         print("Number of hapax legomena:", no_hapax)
-        
+        """
         yule1 = no_tokens
         yule2 = int(np.sum(df_types**2))
         #print(yule2)
         yuleK = 10.000 * (yule2 - yule1) / yule1**2
         #print("Yule's K characteristic for", textname, ":", yuleK)
-        
-        ### Combine all values for one text, combine all texts, save as a table. 
+
+        ### Combine all values for one text, combine all texts, save as a table.
         columns = [textname]
         index = ["no_sentences", "no_types", "no_tokens", "no_tokenchars", "avg_tokenlength", "avg_sentencelength", "ttr", "yuleK"]
         #print(index)
@@ -102,18 +104,19 @@ def basic_stats(file):
         #print(results)
         output = pd.Series(results, index=index, name=textname)
         #print(output)
-        
-        all_output = pd.DataFrame(output)
+
+
+        all_output = empty.append(output, ignore_index=False)
         #print(all_output)
-        
-        with open("all_output.csv", "w") as outfile: 
+
+        with open("all_output.csv", "w") as outfile:
             all_output.to_csv(outfile)
-    
-    
-    
-           
-        
-        
+
+
+
+
+
+
 
 #######################
 # Main                #
