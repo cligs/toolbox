8b162b027d38ed25fd2aa3e09e4c4d96968b9faf
Christof Schöch <c.schoech@gmail.com>
2015-07-10 11:20:37 +0200

    create outfolder, set parameters

--- extract/teireader.py
+++ extract/teireader.py
@@ -9,56 +9,80 @@ import re
 import os
 import glob
 from lxml import etree
-
-def teireader(inpath):
+ 
+def teireader(wdir, inpath, outfolder, xpath):
     """Script for reading selected text from TEI files."""
+    inpath  = wdir + inpath
+    outfolder = wdir + outfolder
+    if not os.path.exists(outfolder):
+        os.makedirs(outfolder)
+    ## Do the following for each file in the inpath.
     for file in glob.glob(inpath):
         with open(file, "r") as infile:
             filename = os.path.basename(file)[:-4]
-            idno = filename[:5]
-            print(idno)
+            ## idno must be at the beginning of the filename and have 6 characters.
+            idno = filename[:6]
+            print("Treating " + idno)
             xml = etree.parse(file)
             namespaces = {'tei':'http://www.tei-c.org/ns/1.0'}
 
             ### Removes tags but conserves their text content.
+            ### USER: Uncomment as needed.
+            #etree.strip_tags(xml, "{http://www.tei-c.org/ns/1.0}seg")
+            #etree.strip_tags(xml, "{http://www.tei-c.org/ns/1.0}said")
             #etree.strip_tags(xml, "{http://www.tei-c.org/ns/1.0}hi")
 
             ### Removes elements and their text content.
+            ### USER: Uncomment as needed.
             #etree.strip_elements(xml, "{http://www.tei-c.org/ns/1.0}reg")
             #etree.strip_elements(xml, "{http://www.tei-c.org/ns/1.0}orig")
-            #etree.strip_elements(xml, "{http://www.tei-c.org/ns/1.0}note")
+            etree.strip_elements(xml, "{http://www.tei-c.org/ns/1.0}note")
             #etree.strip_elements(xml, "{http://www.tei-c.org/ns/1.0}l")
             #etree.strip_elements(xml, "{http://www.tei-c.org/ns/1.0}p")
-            #etree.strip_elements(xml, "{http://www.tei-c.org/ns/1.0}head")
+            etree.strip_elements(xml, "{http://www.tei-c.org/ns/1.0}head")
             #etree.strip_elements(xml, "{http://www.tei-c.org/ns/1.0}stage")
-            #etree.strip_elements(xml, "{http://www.tei-c.org/ns/1.0}speaker")
+            etree.strip_elements(xml, "{http://www.tei-c.org/ns/1.0}speaker")
 
             ### XPath defining which text to select
-            xp_bodyprose = "//tei:body//tei:p//text()"
-            xp_bodyverse = "//tei:body//tei:l//text()"
             xp_bodytext = "//tei:body//text()"
             xp_alltext = "//text()"
-            xp_castlist = "//tei:castList//text()"
-            xp_stage = "//tei:stage//text()"
-            xp_hi = "//tei:body//tei:hi//text()"
-
+            xp_seg = "//tei:body//tei:seg//text()"
+            xp_said = "//tei:body//tei:said//text()"
+            #xp_bodyprose = "//tei:body//tei:p//text()"
+            #xp_bodyverse = "//tei:body//tei:l//text()"
+            #xp_castlist = "//tei:castList//text()"
+            #xp_stage = "//tei:stage//text()"
+            #xp_hi = "//tei:body//tei:hi//text()"
+            
             ### Applying one of the above XPaths
-            text = xml.xpath(xp_bodytext, namespaces=namespaces)
+            ### USER: use on of the xpath values used here in the parameters.
+            if xpath == "bodytext": 
+                text = xml.xpath(xp_bodytext, namespaces=namespaces)
+            if xpath == "alltext": 
+                text = xml.xpath(xp_alltext, namespaces=namespaces)
+            if xpath == "seg": 
+                text = xml.xpath(xp_seg, namespaces=namespaces)
+            if xpath == "said": 
+                text = xml.xpath(xp_said, namespaces=namespaces)
             text = "\n".join(text)
 
             ### Some cleaning up
-            text = re.sub("  ", "", text)
-            #text = re.sub("    ", "", text)
-            #text = re.sub("\n{1,6}", "", text)
-            text = re.sub("\n{1,6}", "\n", text)
-            text = re.sub("\n{1,6}", "\n", text)
+            text = re.sub("[ ]{2,8}", "", text)
+            text = re.sub("\n{2,8}", "", text)
+            text = re.sub("[ \n]{2,8}", "", text)
+            text = re.sub("\t{1,8}", "", text)
 
+            # TODO: Improve whitespace handling.
+            
             outtext = str(text)
-            outfile = "./txt/" + filename + ".txt"
+            outfile = outfolder + filename + ".txt"
         with open(outfile,"w") as output:
             output.write(outtext)
 
-def main(inpath):
-    teireader(inpath)
+def main(wdir, inpath, outfolder, xpath):
+    teireader(wdir, inpath, outfolder, xpath)
 
-main("./tei/*.xml")
+main("/home/christof/Repos/cligs/romanlumieres/", "master/*.xml", "txt/", "bodytext")
+
+
+
