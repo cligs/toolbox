df73cb5cafbb9146ad8c4012c065b7d1c6944b4c
Christof Schöch <c.schoech@gmail.com>
2016-01-26 14:07:23 +0100

    Added module crawl

--- activate_toolbox.py
+++ activate_toolbox.py
@@ -14,10 +14,14 @@ import os
 ## Enter the path to the folder in which your toolbox folder is located.
 ## Example: sys.path.append(os.path.abspath("/home/christof/Repos/cligs/"))
 sys.path.append(os.path.abspath("/home/christof/Repos/cligs/"))
-
-## This is for checking whether the path was added correctly.
-print(sys.path)
+#sys.path.append(os.path.abspath("/usr/local/lib/python3.4/dist-packages/"))
+#sys.path.append(os.path.abspath("/usr/local/lib/python3.4/dist-packages/numpy-1.10.4.egg"))
 
 ## Optional: Activate to remove a (mistaken or redundant path)    
-#sys.path.remove(os.path.abspath("//"))
-#print(sys.path)
+#sys.path.remove(os.path.abspath("/usr/local/lib/python3.4/dist-packages/"))
+#sys.path.remove(os.path.abspath("/home/christof/Repos/cligs/"))
+#sys.path.remove(os.path.abspath("/usr/lib/python3.4/"))
+#sys.path.remove(os.path.abspath("/usr/local/lib/python3.4/dist-packages/numpy-1.10.4.egg"))
+
+## This is for checking whether the path settings are correct.
+print(sys.path)
