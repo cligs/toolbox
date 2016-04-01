80da3cf0de9aaf97e369503be37ba6df2abd5588
morethanbooks <jose.calvo@morethanbooks.eu>
2016-04-01 15:26:11 +0200

    New function in assist metadata and some bugs killed

--- assist_metadata.py
+++ assist_metadata.py
@@ -12,7 +12,7 @@ from lxml import etree
 
 """
 To use all of them just use:
-df = get_all("/home/jose/cligs/ne/master/", "ne0002")
+df = get_all("/home/jose/cligs/ne/master/", "ne0030")
 """
 
 def get_names(wdir, txtFolder):
@@ -38,11 +38,11 @@ def get_names(wdir, txtFolder):
             names=[]
             # We search for any word that starts with capital letter and that before didn't have anything that looks like an starting of a sentence
             names = re.findall(r'(?<=[a-zá-úñüç,;] )([A-ZÁ-ÚÜÑ][a-zá-úñüç]+)', content)
-            print(names)
+            #print(names)
 
             # We delete the duplicated items in the list            
             names=list(set(names))
-            print(names)
+            #print(names)
             
             # Now we put the list in a data frame
             df=pd.DataFrame(names,columns=["name"])
@@ -62,6 +62,51 @@ def get_names(wdir, txtFolder):
             # The data is printed as a file
             #df.to_csv(wdir+txtFolder+'_ProperNames.csv', sep='\t', encoding='utf-8')
         return df
+
+def get_full_names(wdir, txtFolder):
+    """
+
+    wdir is the path of the gile
+    txtFolder is the name (without format ending) of the file to be analized
+
+    Example of how to use it at the console:
+    
+    df = get_full_names("/home/jose/cligs/ne/master/", "ne0032")
+    """
+    #Lets open the file
+    for doc in glob.glob(wdir+txtFolder+"*"):
+        
+        with open(doc, "r", errors="replace", encoding="utf-8") as fin:
+            content = fin.read()
+
+            # We delimit that we only want to find information in the text() of XML-TEI
+            content = fin.read()
+            xml = etree.parse(doc)
+            namespaces = {'tei':'http://www.tei-c.org/ns/1.0'}
+            xp_bodytext = "//tei:body//text()"
+            content = xml.xpath(xp_bodytext, namespaces=namespaces)
+            # etree gives us a string where each line a string; but we only want a string:          
+            content = ' '.join(content)
+            
+            #We create a list for the places
+            fullNames = []
+
+
+            if re.search(r'(?<=[a-zá-úñüç,;] )([A-ZÁ-ÚÜÑ][a-zá-úñüç]+(?:(?: del | del | de la | )[A-ZÁ-ÚÜÑ][a-zá-úñüç]+)+)', content) is None:
+                print("no fullNames found (weird!!!) \n:(\n\n\n")
+            else:
+                print("\nyey! We found some fullNames :)\n")
+
+                # We search for any word that starts with capital letter and that before had the Spanish preposition "de" or "en" (an intuitiv thing I though myself)
+                fullNames = re.findall(r'(?<=[a-zá-úñüç,;] )([A-ZÁ-ÚÜÑ][a-zá-úñüç]+(?:(?: del | del | de la | )[A-ZÁ-ÚÜÑ][a-zá-úñüç]+)+)', content)
+                countfullNames = Counter(fullNames)
+
+                dfCountFullNames = pd.DataFrame.from_dict(countfullNames, orient='index').reset_index()
+                dfCountFullNames = dfCountFullNames.rename(columns={'index':'Year', 0:'freq'})
+                dfCountFullNames = dfCountFullNames.sort(["freq"], ascending=True)        
+                print(dfCountFullNames)
+        
+                return dfCountFullNames
 
 def get_places(wdir, txtFolder):
     """
@@ -90,29 +135,23 @@ def get_places(wdir, txtFolder):
             
             #We create a list for the places
             places = []
-            # We search for any word that starts with capital letter and that before had the Spanish preposition "de" or "en" (an intuitiv thing I though myself)
-            places = re.findall(r'(?:en|de|En|De )([A-ZÁ-ÚÜÑ][a-zá-úñüç]+)', content)
-
-            # We delete the duplicated items in the list            
-            places = list(set(places))
-            
-            # Now we put the list in a data frame
-            df = pd.DataFrame(places,columns=["place"])
-
-            #And we add a new column for the frequency and we fill it with zeros
-            df["freq"] = 0
 
 
-            # Now, for every row, we take the indexes and the other columns with the real values (names and frequency)            
-            for index, row in df.iterrows():
-                # For each, we fill the frecuency with the the amount (len) of a times that the name appears in the text with something 
-                df.at[index,"freq"] = len(re.findall(r'[^a-zá-úçñüA-ZÁ-ÚÜÑ\-]'+ re.escape(row["place"]) + r'[^a-zá-úçñüA-ZÁ-ÚÜÑ\-]', content))
+            if re.search(r'(?:en|En) ([A-ZÁ-ÚÜÑ][a-zá-úñüç]+)', content) is None:
+                print("no place found :(\n\n\n")
+            else:
+                print("\nyey! We found some years :)\n")
 
-            df=df.sort(["freq"], ascending=True)
-            # The df is sorted after the frequency
-            print(df)
+                # We search for any word that starts with capital letter and that before had the Spanish preposition "de" or "en" (an intuitiv thing I though myself)
+                places = re.findall(r'(?:en|En) ([A-ZÁ-ÚÜÑ][a-zá-úñüç]+)', content)
+                countPlaces = Counter(places)
 
-        return df
+                dfCountPlaces = pd.DataFrame.from_dict(countPlaces, orient='index').reset_index()
+                dfCountPlaces = dfCountPlaces.rename(columns={'index':'Year', 0:'freq'})
+                dfCountPlaces = dfCountPlaces.sort(["freq"], ascending=True)        
+                print(dfCountPlaces)
+        
+                return dfCountPlaces
 
 
 def get_time(wdir, txtFolder):
@@ -147,7 +186,7 @@ def get_time(wdir, txtFolder):
 
             # We search if there are numbers with four digits in the text at all
             if re.search(r'\D(\d\d\d\d)\D', content) is None:
-                print("no year found :(")
+                print("no year found :(\n\n\n")
             else:
                 print("\nyey! We found some years :)\n")
                 
@@ -185,5 +224,6 @@ def get_all(wdir, txtFolder):
         df = get_all("/home/jose/cligs/ne/master/", "ne0002")
     """
     get_names(wdir, txtFolder)
+    get_full_names(wdir, txtFolder)
     get_time(wdir, txtFolder)
     get_places(wdir, txtFolder)
