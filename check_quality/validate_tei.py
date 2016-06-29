303410b64b08deee8cad0e8a2f24ba1ecaeda688
Ulrike Henny <ulrike.henny@uni-koeln.de>
2016-06-29 10:33:05 +0200

    update validate tei

--- check_quality/validate_tei.py
+++ check_quality/validate_tei.py
@@ -7,14 +7,21 @@ import glob
 from lxml import etree
 import sys
 
-teipath = "./testtei/tc*.xml"
-rngfile = "cligs.rng"
 
-def validate_tei(teipath, rngfile):
+def main(teipath, rngfile):
+	"""
+	Arguments:
+	teipath (str): path to the TEI files, e.g. /home/ulrike/Dokumente/Git/textbox/es/novela-espanola/tei/*.xml
+	rngfile (str): path to the schema file, e.g. /home/ulrike/Schreibtisch/basisformat.rng
+	
+	Example:
+	from toolbox import check_quality
+	check_quality.validate_tei("/home/ulrike/Dokumente/Git/textbox/es/novela-espanola/tei/*.xml", "/home/ulrike/Schreibtisch/basisformat.rng")
+	"""
     for teifile in glob.glob(teipath): 
         idno = os.path.basename(teifile)
         #print(idno)
-        rngparsed = etree.parse("cligs.rng")
+        rngparsed = etree.parse(rngfile)
         rngvalidator = etree.RelaxNG(rngparsed)
         parser = etree.XMLParser(recover=True)
         teiparsed = etree.parse(teifile, parser)
@@ -25,11 +32,11 @@ def validate_tei(teipath, rngfile):
             print(idno, "valid!")
         else:
             print(idno, "sorry, not valid!")
+            print(log)
             #print(log.last_error)
             #print(log.last_error.domain_name)
+            #print(log.last_error.type_name)
 
-def main(teipath, rngfile):
-    validate_tei(teipath, rngfile)
 
 if __name__ == "__main__":
     main(int(sys.argv[1])) 
