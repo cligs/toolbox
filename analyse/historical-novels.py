97d1d455d315d537bb870fb1320d0a4e8864b138
hennyu <ulrike.henny@uni-koeln.de>
2016-10-25 15:53:57 +0200

    update hist script

--- analyse/historical-novels.py
+++ analyse/historical-novels.py
@@ -13,10 +13,13 @@ Based on:
 import sys
 import os
 import pandas as pd
+import numpy as np
 from lxml import etree
 import glob
 import re
 import copy
+import math
+import matplotlib.pyplot as plt
 
 # use the following to add the toolbox to syspath (if needed):
 sys.path.append(os.path.abspath("/home/ulrike/Git/"))
@@ -31,8 +34,11 @@ The data which is analyzed here has been previously annotated with the following
 	annotate/workflow_teifw.py
 """
 
+# path and filename settings
 wdir = "/home/ulrike/Dokumente/Konferenzen/DH/2017/"
-
+md_mode = "hist-nov"
+md_csv = "metadata_" + md_mode + ".csv"
+dir_visuals = os.path.join(wdir, "vis")
 
 def summarize_corpus():
 	"""
@@ -44,9 +50,7 @@ def summarize_corpus():
 	"""
 	
 	# get metadata
-	md_mode = "hist-nov"
 	get_metadata.from_TEIP5(wdir,"corpora/base/*/*/*.xml", "metadata", md_mode)
-	md_csv = "metadata_" + md_mode + ".csv" 
 	
 	# visualize some metadata
 	visualize_metadata.describe_corpus(wdir, md_csv, "author-continent")
@@ -57,7 +61,7 @@ def summarize_corpus():
 	visualize_metadata.plot_pie(wdir, md_csv, "subgenre_x")
 	
 	# make some counts
-	md_table = pd.DataFrame.from_csv(wdir + "metadata_hist-nov.csv", header=0)
+	md_table = pd.DataFrame.from_csv(os.path.join(wdir, "metadata_hist-nov.csv"), header=0)
 	num_texts = len(md_table)
 	num_language = len(md_table.groupby(["language"]))
 	num_continent = len(md_table.groupby(["author-continent"]))
@@ -78,16 +82,12 @@ def summarize_corpus():
 	"num_not_historical":[num_not_historical]}
 	
 	count_fr = pd.DataFrame(d)
-	count_fr.to_csv(wdir + "corpus-counts.csv", sep=",", header=True)
-	print("Done")
+	count_fr.to_csv(os.path.join(wdir, "corpus-counts.csv"), sep=",", header=True)
+	print("Done: summarize corpus")
 	
-
-
-def generate_hdt_features():
-	"""
-	Generates features based on the results of annotation with HeidelTime
-	
-	Documentation of labels:
+################################## Temporal expression features #####################################
+"""
+Documentation of labels:
 	idno: text id in CLiGS project
 	
 	- absolute values -
@@ -106,8 +106,8 @@ def generate_hdt_features():
 	tpx_date_full_abs: number of fully specified DATE expressions (YEAR, MONTH, DAY)
 
 	tpx_date_chapter_first_abs: number of DATE expressions in the first chapter of the novel
-	tpx_date_chapter_other_abs: mean of DATE expressions in the remaining chapters of the novel 
-	
+	tpx_date_chapter_other_mean_abs: mean of DATE expressions in the remaining chapters of the novel 
+	tpx_date_chapter_all_mean_abs: mean of DATE expressions in all chapters of the novel
 
 	- relative values -
 	(explanations see above; all values relative to the total number of words in the text)
@@ -126,7 +126,8 @@ def generate_hdt_features():
 	tpx_date_full_rel
 
 	tpx_date_chapter_first_rel
-	tpx_date_chapter_other_rel
+	tpx_date_chapter_other_mean_rel
+	tpx_date_chapter_all_mean_rel
 	
 	
 	- proportinal values -
@@ -138,125 +139,151 @@ def generate_hdt_features():
 	tpx_set_prop: proportion of SET expressions
 	tpx_date_full_prop: proportion of fully specified DATE expressions
 	tpx_date_chapter_first_prop: proportion of DATE expressions in the first chapter of the novel
+	tpx_date_chapter_other_mean_prop: mean of the proportion of DATE expressions in the remaining chapters of the novel
+	tpx_date_chapter_all_mean_prop: mean of the proportion of DATE expressions in the all chapters of the novel
+"""
+# labels for tpx data frame
+labels_abs = ["tpx_all_abs", "tpx_date_abs", "tpx_time_abs", "tpx_duration_abs", "tpx_set_abs", "tpx_date_year_abs", 
+"tpx_date_year_month_abs", "tpx_date_month_abs", "tpx_date_day_abs", "tpx_date_month_day_abs", "tpx_date_full_abs", 
+"tpx_date_chapter_first_abs", "tpx_date_chapter_other_mean_abs", "tpx_date_chapter_all_mean_abs"]
+
+labels_rel = ["tpx_all_rel", "tpx_date_rel", "tpx_time_rel", "tpx_duration_rel", "tpx_set_rel", "tpx_date_year_rel", 
+"tpx_date_year_month_rel", "tpx_date_month_rel", "tpx_date_day_rel", "tpx_date_month_day_rel", "tpx_date_full_rel", 
+"tpx_date_chapter_first_rel", "tpx_date_chapter_other_mean_rel", "tpx_date_chapter_all_mean_rel"]
+
+labels_prop = ["tpx_date_prop", "tpx_time_prop", "tpx_duration_prop", "tpx_set_prop", "tpx_date_full_prop", 
+"tpx_date_chapter_first_prop", "tpx_date_chapter_other_mean_prop", "tpx_date_chapter_all_mean_prop"]
+
+labels = copy.copy(labels_abs) + copy.copy(labels_rel) + copy.copy(labels_prop)
+
+
+
+
+def generate_tpx_features():
+	"""
+	Generates features based on the results of annotation with HeidelTime
 	"""
 
+	# path to XML files annotated with HeidelTime
+	ht_inpath = os.path.join(wdir, "corpora/hdt_chapterwise/*.xml")
+
+	labels.append("num_words")
+
+	# read existing metadata
+	md_table = pd.DataFrame.from_csv(wdir + md_csv, header=0)
+	idnos = md_table.idno
+
+	# crate new data frame
+	ht_fr = pd.DataFrame(columns=labels, index=idnos)
+	 
+	# XPath expressions for TimeML requests
+	namespaces = {'tei':'http://www.tei-c.org/ns/1.0'}
+
+	xpaths = {"tpx_all_abs" : "count(//TIMEX3)",
+	"tpx_date_abs" : "count(//TIMEX3[@type='DATE'])",
+	"tpx_time_abs" : "count(//TIMEX3[@type='TIME'])",
+	"tpx_duration_abs" : "count(//TIMEX3[@type='DURATION'])",
+	"tpx_set_abs" : "count(//TIMEX3[@type='SET'])",
+	"tpx_date_year_abs" : "count(//TIMEX3[@type='DATE'][substring(@value,1,1) != 'X' and substring(@value,2,1) != 'X' and substring(@value,3,1) != 'X' and substring(@value,4,1) != 'X'])",
+	"tpx_date_year_month_abs" : "count(//TIMEX3[@type='DATE'][substring(@value,1,1) != 'X' and substring(@value,2,1) != 'X' and substring(@value,3,1) != 'X' and substring(@value,4,1) != 'X' and substring(@value,6,1) != 'X' and substring(@value,7,1) != 'X'])",
+	"tpx_date_month_abs" : "count(//TIMEX3[@type='DATE'][substring(@value,6,1) != 'X' and substring(@value,7,1) != 'X'])",
+	"tpx_date_day_abs" : "count(//TIMEX3[@type='DATE'][substring(@value,9,1) != 'X' and substring(@value,10,1) != 'X'])",
+	"tpx_date_month_day_abs" : "count(//TIMEX3[@type='DATE'][substring(@value,6,1) != 'X' and substring(@value,7,1) != 'X' and substring(@value,9,1) != 'X' and substring(@value,10,1) != 'X'])",
+	"tpx_date_full_abs" : "count(//TIMEX3[@type='DATE'][substring(@value,1,1) != 'X' and substring(@value,2,1) != 'X' and substring(@value,3,1) != 'X' and substring(@value,4,1) != 'X' and substring(@value,6,1) != 'X' and substring(@value,7,1) != 'X' and substring(@value,9,1) != 'X' and substring(@value,10,1) != 'X'])",
+	"tpx_date_chapter_first_abs" : "count(//TIMEX3[@type='DATE'][substring(ancestor::tei:div/@xml:id,string-length(ancestor::tei:div/@xml:id) - 1,2) ='d1'])",
+	"tpx_date_chapter_other_mean_abs" : "count(//TIMEX3[@type='DATE'][substring(ancestor::tei:div/@xml:id,string-length(ancestor::tei:div/@xml:id) - 1,2) !='d1']) div (count(//wrapper) - 1)",
+	"tpx_date_chapter_all_mean_abs" : "count(//TIMEX3[@type='DATE']) div count(//wrapper)"}
 
 
-# path to XML files annotated with HeidelTime
-ht_inpath = wdir + "corpora/hdt_chapterwise/*.xml"
 
-# labels for data frame
-labels_abs = ["tpx_all_abs", "tpx_date_abs", "tpx_time_abs", "tpx_duration_abs", "tpx_set_abs"]
-"""
-, "tpx_date_year_abs", 
-"tpx_date_year_month_abs", "tpx_date_month_abs", "tpx_date_day_abs", "tpx_date_month_day_abs", "tpx_date_full_abs", 
-"tpx_date_chapter_first_abs", "tpx_date_chapter_other_abs"]
-"""
-labels_rel = ["tpx_all_rel", "tpx_date_rel", "tpx_time_rel", "tpx_duration_rel", "tpx_set_rel"]
-""" 
-, "tpx_date_year_rel", 
-"tpx_date_year_month_rel", "tpx_date_month_rel", "tpx_date_day_rel", "tpx_date_month_day_rel", "tpx_date_full_rel", 
-"tpx_date_chapter_first_rel", "tpx_date_chapter_other_rel"
- """
-labels_prop = ["tpx_date_prop", "tpx_time_prop", "tpx_duration_prop", "tpx_set_prop"]
-""" 
- "tpx_date_full_prop", "tpx_date_chapter_first_prop" 
- """
-labels = copy.copy(labels_abs) #+ labels_rel + labels_prop
-labels.append("num_words")
-
-
-# read existing metadata
-md_table = pd.DataFrame.from_csv(wdir + "metadata_hist-nov.csv", header=0)
-idnos = md_table.idno
-
-# crate new data frame
-ht_fr = pd.DataFrame(columns=labels, index=idnos)
- 
-# XPath expressions for TimeML requests
-xpaths = {"tpx_all_abs" : "count(//TIMEX3)",
-"tpx_date_abs" : "count(//TIMEX3[@type='DATE'])",
-"tpx_time_abs" : "count(//TIMEX3[@type='TIME'])",
-"tpx_duration_abs" : "count(//TIMEX3[@type='DURATION'])",
-"tpx_set_abs" : "count(//TIMEX3[@type='SET'])"}
-""",
-"tpx_date_year_abs" : "count(//TIMEX3[@type='DATE'][substring(@value,1,1) != 'X' and substring(@value,2,1) != 'X' and substring(@value,3,1) != 'X' and substring(@value,4,1) != 'X'])",
-"tpx_date_year_month_abs" : "count(//TIMEX3[@type='DATE'][substring(@value,1,1) != 'X' and substring(@value,2,1) != 'X' and substring(@value,3,1) != 'X' and substring(@value,4,1) != 'X' and substring(@value,6,1) != 'X' and substring(@value,7,1) != 'X')])",
-"tpx_date_month_abs" : "count(//TIMEX3[@type='DATE'][substring(@value,6,1) != 'X' and substring(@value,7,1) != 'X'])",
-"tpx_date_day_abs" : "count(//TIMEX3[@type='DATE'][substring(@value,9,1) != 'X' and substring(@value,10,1) != 'X'])",
-"tpx_date_month_day_abs" : "count(//TIMEX3[@type='DATE'][substring(@value,6,1) != 'X' and substring(@value,7,1) != 'X' and substring(@value,9,1) != 'X' and substring(@value,10,1) != 'X'])",
-"tpx_date_full_abs" : "count(//TIMEX3[@type='DATE'][substring(@value,1,1) != 'X' and substring(@value,2,1) != 'X' and substring(@value,3,1) != 'X' and substring(@value,4,1) != 'X' and substring(@value,6,1) != 'X' and substring(@value,7,1) != 'X' and substring(@value,9,1) != 'X' and substring(@value,10,1) != 'X'])",
-"tpx_date_chapter_first_abs" : "count(//TIMEX3[@type='DATE'][ends-with(parent::div/@xml:id,'d1')])",
-"tpx_date_chapter_other_abs" : "count(//TIMEX3[@type='DATE'][not(ends-with(parent::div/@xml:id,'d1'))]) div (count(//wrapper) - 1)" """
-
-# loop through files to get HeidelTime results, first step: absolute values
-# subsequent steps build on absolute values
-for file in glob.glob(ht_inpath):
-	
-	idno = os.path.basename(file)[0:6]
-	xml = etree.parse(file)
-	
-	# apply xpaths
-	for label in labels_abs:
-		xpath = xpaths[label]
-		result = xml.xpath(xpath)
+	# loop through files to get HeidelTime results, first step: absolute values
+	# subsequent steps build on absolute values
+	for file in glob.glob(ht_inpath):
 		
-		# Write the result into the data frame
-		ht_fr.loc[idno,label] = result
-
-
-# second step: relative values (relative to the total number of words in the text)
-for file in glob.glob(ht_inpath):
-	
-	idno = os.path.basename(file)[0:6]
-	
-	# calculate total number of words in the text
-	num_words = 0
-	xml = etree.parse(file)
-	# get XML snippets chapterwise
-	wrappers = xml.xpath("//wrapper//text()")
-	for wrap in wrappers:
+		idno = os.path.basename(file)[0:6]
+		xml = etree.parse(file)
 		
-		# tokenize and count
-		words = re.split(r"[\s\n]+", wrap)
-		num_words += len(words)
-	
-	ht_fr.loc[idno,"num_words"] = num_words
-	
-	# apply xpaths
-	for label in labels_rel:
-		# set corresponding absolute value label
-		label_abs = label[:-3] + "abs"
+		# apply xpaths
+		for label in labels_abs:
+			xpath = xpaths[label]
+			result = xml.xpath(xpath, namespaces=namespaces)
+			
+			# check the results of XPath
+			"""
+			if math.isnan(result):
+				result = "is not a number"
+			"""
+			
+			# Write the result into the data frame
+			ht_fr.loc[idno,label] = result
+
+
+	# second step: relative values (relative to the total number of words in the text)
+	for file in glob.glob(ht_inpath):
 		
-		# calculate relative value
-		result = ht_fr.loc[idno,label_abs] / num_words
+		idno = os.path.basename(file)[0:6]
 		
-		# Write the result into the data frame
-		ht_fr.loc[idno,label] = result
-
-
-# third step: calculate proportions
-for file in glob.glob(ht_inpath):
-	
-	idno = os.path.basename(file)[0:6]
-	tpx_all = ht_fr.loc[idno,"tpx_all_abs"]
-	tpx_all_one = tpx_all / 100
-	
-	for label in labels_prop:
-		# set corresponding absolute value label
-		label_abs = label[:-4] + "abs"
+		# calculate total number of words in the text
+		num_words = 0
+		xml = etree.parse(file)
+		# get XML snippets chapterwise
+		wrappers = xml.xpath("//wrapper//text()")
+		for wrap in wrappers:
+			
+			# tokenize and count
+			words = re.split(r"[\s\n]+", wrap)
+			num_words += len(words)
 		
-		# calculate proportion
-		result = ht_fr.loc[idno,label_abs] / tpx_all_one
-	
-		# Write the result into the data frame
-		ht_fr.loc[idno,label] = result
-	
+		ht_fr.loc[idno,"num_words"] = num_words
+		
+		# apply xpaths
+		for label in labels_rel:
+			# set corresponding absolute value label
+			label_abs = label[:-3] + "abs"
+			
+			# fetch absolute value
+			abs_val = ht_fr.loc[idno,label_abs]
+			
+			# check data type
+			if math.isnan(abs_val):
+				result = abs_val
+			else:
+				# calculate relative value
+				result = abs_val  / num_words
+			
+			
+			# Write the result into the data frame
+			ht_fr.loc[idno,label] = result
 
 
-print(ht_fr)
-ht_fr.to_csv(wdir + "corpus-counts-hdt.csv", sep=",", header=True)
+	# third step: calculate proportions
+	for file in glob.glob(ht_inpath):
+		
+		idno = os.path.basename(file)[0:6]
+		tpx_all = ht_fr.loc[idno,"tpx_all_abs"]
+		tpx_all_one = tpx_all / 100
+		
+		for label in labels_prop:
+			# set corresponding absolute value label
+			label_abs = label[:-4] + "abs"
+			
+			# fetch absolute value
+			abs_val = ht_fr.loc[idno,label_abs]
+			
+			# check data type
+			if math.isnan(abs_val):
+				result = abs_val
+			else:
+				# calculate proportion
+				result = abs_val / tpx_all_one
+			
+			# Write the result into the data frame
+			ht_fr.loc[idno,label] = result
+		
 
+	ht_fr.to_csv(wdir + "corpus-counts-hdt.csv", sep=",", header=True)
+
+	print("Done: generate tpx features")
 
 
 
@@ -266,7 +293,6 @@ freeling-Ergebnisse auswerten
 -- Zahl der Wörter, Sätze
 -- TXM?
 """
-
 
 
 """
@@ -280,8 +306,80 @@ Kombination verschiedener Merkmale
 
 
 """
-Visualisieren einzelner Features
+visualize the distribution of specific feature values
 """
+
+def plot_scatter(tpx_feature):
+	"""
+	Make a scatter plot showing the number of specific temporal expressions in historical vs. non-historical novels
+	
+	Arguments:
+	tpx_feature (string): Name of the temporal expression feature to plot 
+	"""
+
+	md_table = pd.DataFrame.from_csv(os.path.join(wdir, md_csv), header=0)
+	ht_table = pd.DataFrame.from_csv(os.path.join(wdir, "corpus-counts-hdt.csv"), header=0)
+	working_table = ht_table.join(md_table)
+
+	# z.B. tpx_date_full_rel
+	# get data points and sort
+	data = copy.copy(working_table[tpx_feature])
+	#replace(to_replace="na", value=0)
+	data_sorted = data.sort_values(ascending=False)
+
+	# get ids of historical novels
+	idnos_hist = md_table[md_table["subgenre_hist"] == "historical"].index.tolist()
+	# get ids of non-historical novels
+	idnos_not_hist = md_table[md_table["subgenre_hist"] == "not_historical"].index.tolist()
+
+	# split data into subgroups
+	data_hist = data[idnos_hist]
+	data_not_hist = data[idnos_not_hist]
+		
+
+	# get ranks
+	ranks = {}
+	for idx, val in enumerate(data_sorted.index):
+		ranks[val] = idx
+
+	# visualize as scatterplot
+	plt.figure(figsize=(20,6))
+
+				# rank as x values, alternative: range(len(data_hist))
+	plt.scatter([ranks[idno] for idno in idnos_hist],
+				# counts as y values
+				data_hist,
+				marker = "D",
+				color = "#3366CC",
+				alpha = 1,
+				s = 50,
+				label = tpx_feature + ", historical novel"
+	)
+
+	plt.scatter([ranks[idno] for idno in idnos_not_hist],
+				# counts as y values
+				data_not_hist,
+				marker = "o",
+				color = "#DC3912",
+				alpha = 1,
+				s = 50,
+				label = tpx_feature + ", non-historical novel"
+	)
+	plt.title("Novels and number of temporal expressions (TPX)")
+	plt.ylabel("Number of TPX")
+	plt.xlabel("Novel rank")
+	plt.xlim(-5,len(data) + 5)
+
+	plt.legend(loc='upper right')
+	plt.tight_layout()
+
+	figurename = "scatter-"+ tpx_feature +".png"
+	plt.savefig(os.path.join(dir_visuals, figurename), dpi=300)
+	plt.close()
+	
+	print("Plotted " + figurename)
+
+
 
 """
 Signifikanz-Tests für einzelne Features
@@ -294,5 +392,13 @@ Anwendung: Clustern oder Masch.Lernen mit Features
 
 
 # summarize_corpus()
+# generate_tpx_features()
+# make scatter plots for all tpx-features
+""" 
+for feature in labels:
+	plot_scatter(feature)
+"""
+
+
 
 
