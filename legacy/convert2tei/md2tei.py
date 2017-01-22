5340d568103f8c75ae412b74e73577a8a34bf033
christofs <c.schoech@gmail.com>
2017-01-22 15:30:10 +0100

    cleaner version

--- legacy/convert2tei/md2tei.py
+++ legacy/convert2tei/md2tei.py
@@ -1,161 +1,88 @@
 #!/usr/bin/env python3
 # Filename: md2tei.py
 
+
 """
-# Function to transform html files from Markdown into simple, clean TEI files.
+# Function to transform simple Markdown files into simple, clean TEI files.
 """
+
 
 import re
 import glob
 import os
 
 
-#######################
-# Functions           #
-#######################
+filepath = "./md/*.txt"
+teiheader = "teiHeader.xml"
 
-def md2tei(file,teiheader):
+
+# ==============================
+# Functions
+# ==============================
+
+
+def read_md(file):
     with open(file,"r") as sourcefile:
-        h_text = sourcefile.read()
-        basename = os.path.basename(file)
-        textname, ext = os.path.splitext(basename)
-        print("Now working on: ", textname)
+        md = sourcefile.read()
+        filename, ext = os.path.basename(file).split(".")
+        print("Now working on: ", filename)
+        return md, filename
 
 
-        #######################################################
-        #### Preliminary text preparation                  ####
-        #######################################################
-
-        # Remove troublemakers
-         
-        #################################################################
-        #### Segmentation into: header, front, body, back, finale.   ####
-        #################################################################
-
-        h_header = h_text[0:5000]
-        
-        h_body = h_text
-        
-        t_back = "<back><div>\n<p></p>\n</div></back>"
-
-        t_finale = "</text></TEI>"
-
-        
-        ##########################################################################################
-        #### Work on the separate text segments                                               ####
-        ##########################################################################################
-
-        #################
-        #### header  ####
-        #################
-
-        # Read empty header
-        with open(teiheader,"r") as infile:
-            t_header = infile.read()
-        
-        #################
-        #### front   ####
-        #################
+def read_header(teiheader):
+    with open(teiheader,"r") as infile:
+        header = infile.read()
+        return header
 
 
-        #################
-        #### body    ####
-        #################
-        
-        # Call it t_body from now
-        t_body = h_body
+def md2tei(md, header):
 
-        # Initial and final body tags.
-        t_body = "<body>\n<div>\n" + str(t_body) + "\n</div>\n</body>"
+    # Define the parts
+    header = header
+    front = "<front>\n<div>\n<p></p>\n</div>\n</front>"
+    body = "\n<body>\n<div>\n<p>" + md + "</p>\n</div>\n</body>"
+    back = "\n<back>\n<div><p></p>\n</div>\n</back>"
+    finale = "\n</text></TEI>"
 
+    # Clean the body
+    body = re.sub("\\\\!", "!", body)
+    body = re.sub("\\\\\(", "(", body)
+    body = re.sub("\\\\\)", ")", body)
+    body = re.sub("<p>---", "<p>--", body)
+    body = re.sub("&", "&amp;", body)
 
-        ## Identify and mark chapter headings, including divisions
-        t_body = re.sub("<p>##([IVXLMC]{1,5})</p>", "</div>\n<div>\n<head>\\1</head>", t_body)
-        t_body = re.sub("<p>##(Chapitre [IVXLMC]{1,5})</p>", "</div>\n<div>\n<head>\\1</head>", t_body)
-        t_body = re.sub("<p>##([0-9]{1,3})</p>", "</div>\n<div>\n<head>\\1</head>", t_body)
-        t_body = re.sub("<p>##(Chapitre [0-9]{1,3})</p>", "</div>\n<div>\n<head>\\1</head>", t_body)
+    # Add inline italics
+    body = re.sub("\*(.*?)\*", "<seg rend=\"italic\">\\1</seg>", body)
 
-        t_body = re.sub("<p>#([IVXLMC]{1,5})</p>", "</div>\n<div>\n<head>\\1</head>", t_body)
-        t_body = re.sub("<p>#(Chapitre [IVXLMC]{1,5})</p>", "</div>\n<div>\n<head>\\1</head>", t_body)
-        t_body = re.sub("<p>#([0-9]{1,3})</p>", "</div>\n<div>\n<head>\\1</head>", t_body)
-        t_body = re.sub("<p>#(Chapitre [0-9]{1,3})</p>", "</div>\n<div>\n<head>\\1</head>", t_body)
+    # Add structure
+    body = re.sub("\n\n", "</p>\n<p>", body)
+    body = re.sub("<p>\#.*? (.*?)</p>", "</div>\n<div>\n<head>\\1</head>", body)
+    body = re.sub("</p>\n<p></p>\n<p></p>\n</div>", "</p>\n</div>", body)
 
-        t_body = re.sub("<p># ([IVXLMC]{1,5})</p>", "</div>\n<div>\n<head>\\1</head>", t_body)
-        t_body = re.sub("<p># (Chapitre [IVXLMC]{1,5})</p>", "</div>\n<div>\n<head>\\1</head>", t_body)
-        t_body = re.sub("<p># ([0-9]{1,3})</p>", "</div>\n<div>\n<head>\\1</head>", t_body)
-        t_body = re.sub("<p># (Chapitre [0-9]{1,3})</p>", "</div>\n<div>\n<head>\\1</head>", t_body)
+    # Turn spaces into milestones
+    body = re.sub("</p>\n<p></p>\n<p>", "</p>\n<milestone unit=\"line\"/>\n<p>", body)
+    body = re.sub("</p>\n<p> </p>\n<p>", "</p>\n<milestone unit=\"line\"/>\n<p>", body)
+    body = re.sub("<milestone unit=\"line\"/>\n<p>.?</p>", "<milestone unit=\"line\"/>", body)
 
-        t_body = re.sub("<p>## \*\*([IVXLMC]{1,5})\*\*</p>", "</div>\n<div>\n<head>\\1</head>", t_body)
-        t_body = re.sub("<p>## \*\*(Chapitre [IVXLMC]{1,5})\*\*</p>", "</div>\n<div>\n<head>\\1</head>", t_body)
-        t_body = re.sub("<p>## \*\*([0-9]{1,3})\*\*</p>", "</div>\n<div>\n<head>\\1</head>", t_body)
-        t_body = re.sub("<p>## \*\*(Chapitre [0-9]{1,3})\*\*</p>", "</div>\n<div>\n<head>\\1</head>", t_body)
+    # Clean up a bit
+    body = re.sub("</head>\n</div>\n<div>\n<head></head>\n<p>", "</head>\n<p>", body)
 
-        t_body = re.sub("<p># \*\*([IVXLMC]{1,5})\*\*</p>", "</div>\n<div>\n<head>\\1</head>", t_body)
-        t_body = re.sub("<p># \*\*(Chapitre [IVXLMC]{1,5})\*\*</p>", "</div>\n<div>\n<head>\\1</head>", t_body)
-        t_body = re.sub("<p># \*\*([0-9]{1,3})\*\*</p>", "</div>\n<div>\n<head>\\1</head>", t_body)
-        t_body = re.sub("<p># \*\*(Chapitre [0-9]{1,3})\*\*</p>", "</div>\n<div>\n<head>\\1</head>", t_body)
-
-        t_body = re.sub("<p>\n# ([IVXLMC]{1,5})</p>", "</div>\n<div>\n<head>\\1</head>", t_body, re.DOTALL, re.MULTILINE)
-        t_body = re.sub("<p>\n# (Chapitre [IVXLMC]{1,5})</p>", "</div>\n<div>\n<head>\\1</head>", t_body, re.DOTALL, re.MULTILINE)
-        t_body = re.sub("<p>\n# ([0-9]{1,3})</p>", "</div>\n<div>\n<head>\\1</head>", t_body, re.DOTALL, re.MULTILINE)
-        t_body = re.sub("<p>\n# (Chapitre [0-9]{1,3})</p>", "</div>\n<div>\n<head>\\1</head>", t_body, re.DOTALL, re.MULTILINE)
-
-        
-        t_body = re.sub("<p></p>\n","",t_body)
-        t_body = re.sub("<p> </p>\n","",t_body)
-        t_body = re.sub("<p>[^<]</p>","",t_body)
-        
-        
-        ## Formerly protected spaces; ignore
-        t_body = re.sub(r'\\!','!',t_body)
-        t_body = re.sub(r'\(','(',t_body)
-        
-        ## Some md formatting
-        t_body = re.sub(r'\*(.*?)\*','<hi rend=\"italic\">\\1</hi>',t_body)
-        t_body = re.sub(r'([a-z])<hi rend=\"italic\"> ','\\1 <hi rend=\"italic\">',t_body)
-        
-       
-        
-        #################
-        #### back    ####
-        #################
-
-        t_back = t_back 
-
-        
-        #################
-        #### finale  ####
-        #################
-
-        t_finale = t_finale
-        
-        
-
-        ####################################################################
-        #### Putting all the separate parts of the text into one        ####
-        ####################################################################
+    # Put the parts together
+    tei = header + front + body + back + finale
+    return tei
 
 
-        ### Concatenate the separate parts of the text into one.
-        t_text = t_header + t_body + t_back + t_finale
-
-        ### Save complete text to new file
-        filename = os.path.basename(file)[:-4] + ".xml"
-        with open("./md_tei/" + filename,"w") as outputfile:
-            outputfile.write(t_text)
+def save_tei(tei, filename):
+    filename = "./md/" + filename + ".xml"
+    with open(filename, "w") as outputfile:
+        outputfile.write(tei)
 
 
-       
+def main(filepath, teiheader):
+    for file in glob.glob(filepath):
+        md, filename = read_md(file)
+        header = read_header(teiheader)
+        tei = md2tei(md, header)
+        save_tei(tei, filename)
 
-
-#######################
-# Main                #
-########################
-
-
-def main(inputpath,teiheader):
-    for file in glob.glob(inputpath):
-        md2tei(file,teiheader)
-
-main('./md/*.txt', "teiHeader.xml")
-
+main(filepath, teiheader)
