665e3c2e33e6ceb2c03008914436624c6ff36697
José Calvo <jose.calvo@morethanbooks.eu>
2018-05-08 12:19:55 +0200

    New version of workflow with server

--- annotate/workflow_teifw.py
+++ annotate/workflow_teifw.py
@@ -19,21 +19,22 @@ Run this file directly.
 ############ Options ##############
 
 # where the TEI master files are
-infolder = "/home/ulrike/Git/textbox/spanish/novela-hispanoamericana/tei/"
+infolder = "/home/jose/cligs/ne/master/"
 
 # where the annotation working files and results should go
-outfolder = "/home/ulrike/Git/textbox/spanish/novela-hispanoamericana/anno/"
+outfolder = "/home/jose/cligs/ne/exported_corpora/anno/"
 
 # language of the texts (possible up to now: fr, es, it, pt)
 lang = "es"
 
+server = True
 
-
+print(infolder)
 import sys
 import os
 
 # use the following to add the toolbox to syspath (if needed):
-sys.path.append(os.path.abspath("/home/ulrike/Git/"))
+#sys.path.append(os.path.abspath("/home/ulrike/Git/"))
 
 from toolbox.annotate import prepare_tei
 from toolbox.annotate import annotate_fw
@@ -41,6 +42,8 @@ from toolbox.annotate import annotate_fw
 
 # by default, it should be enough to change the options above and leave this as is
 
-#prepare_tei.prepare("split", infolder, outfolder)
-#annotate_fw.annotate_fw(os.path.join(outfolder, "txt/*.txt"), os.path.join(outfolder, "fl/"), os.path.join(outfolder, "anno/"), lang)
-#prepare_tei.prepare("merge", outfolder, os.path.join(outfolder, "teia"))
+prepare_tei.prepare("split", infolder, outfolder)
+annotate_fw.annotate_fw(os.path.join(outfolder, "txt/*.txt"), os.path.join(outfolder, "fl/"), os.path.join(outfolder, "anno/"), lang, server)
+prepare_tei.prepare("merge", outfolder, os.path.join(outfolder, "teia"))
+
+print("--- %s seconds ---" % (time.time() - start_time))
